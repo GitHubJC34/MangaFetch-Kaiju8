cf180c2a0396c7ce5b7e47d2c49c094e0061efc0
GitHubJC34 <ramanampisoajeancletiano@gmail.com>
2024-05-10 15:20:43 +0300

    Formater la variable $page

--- main.ps1
+++ main.ps1
@@ -11,7 +11,7 @@ for ($chapitre = $chapitreDebut; $chapitre -le $chapitreFin; $chapitre++) {
     $page = 1
     $urlBase = "https://anime-sama.fr/s1/scans/Kaiju%20N%C2%B08/$chapitre/"
     $url = $urlBase + $page + ".jpg"
-    $cheminDossierChapitre = $cheminBase + "Chapitre" + $chapitre
+    $cheminDossierChapitre = $cheminBase + "Chapitre-" + $chapitre
     
     # Créer un dossier pour le chapitre s'il n'existe pas
     if (-not (Test-Path -Path $cheminDossierChapitre)) {
@@ -24,7 +24,10 @@ for ($chapitre = $chapitreDebut; $chapitre -le $chapitreFin; $chapitre++) {
             # Vérifier si l'image existe
             $response = Invoke-WebRequest -Uri $url -UseBasicParsing -Method Head
             if ($response.StatusCode -eq 200) {
-                $cheminDestination = $cheminDossierChapitre + "\Kaiju8_Chapitre" + $chapitre + "_Page" + $page + ".jpg"
+                # Formater la variable $page avec un remplissage de zéros si nécessaire
+                $pageFormatted = "{0:D2}" -f [int]$page
+
+                $cheminDestination = $cheminDossierChapitre + "\Kaiju8_Chapitre-" + $chapitre + "_Page-" + $pageFormatted + ".jpg"
                 
                 # Télécharger l'image
                 Invoke-WebRequest -Uri $url -OutFile $cheminDestination
@@ -33,7 +36,8 @@ for ($chapitre = $chapitreDebut; $chapitre -le $chapitreFin; $chapitre++) {
                 $page++
                 $url = $urlBase + $page + ".jpg"
             }
-        } catch [System.Net.WebException] {
+        }
+        catch [System.Net.WebException] {
             # Si une erreur 404 est capturée, passer au chapitre suivant
             if ($_.Exception.Response.StatusCode -eq 'NotFound') {
                 break
